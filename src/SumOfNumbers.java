70f1ff808593d413131fa12bccb56f9b7204c81b
pragin <pragin.martin@hotmail.com>
2021-11-01 16:06:23 +0000

    sum of 1 to 20

---
+++ src/SumOfNumbers.java
@@ -0,0 +1,14 @@
+//8. write a program that prints the sum of x ranging from 1 to 20.
+
+
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int i = 1 ; i <= 20; i++){
+            sum = sum + i;
+        }
+
+        System.out.println("The sum of x ranging from 1 to 20  = " + sum);
+
+    }
+}
