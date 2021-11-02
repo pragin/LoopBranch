0de545496caa70255f698370b65b6cab5f1c4f31
pragin <pragin.martin@hotmail.com>
2021-11-02 22:01:41 +0000

    completed code

--- src/FibonacciNums.java
+++ src/FibonacciNums.java
@@ -5,14 +5,22 @@ import java.util.Scanner;
 public class FibonacciNums {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter the value of n : ");
+        System.out.print("Please enter the value of n : ");
         int n = scanner.nextInt();
         //Pass the entered value to the method to print Fibonacci Series
         printSeries(n);
     }
 
     public static void printSeries(int n){
-        for(int i = 1; i<n; i++){
+        int numOne = 0;
+        int numTwo = 1;
+        int numThree;
+        System.out.print(numOne + ", " + numTwo + ", ");
+        for(int i = 2 ; i<=n; i++){
+            numThree = numOne +numTwo;
+            System.out.print(numThree+ ", ");
+            numOne = numTwo;
+            numTwo = numThree;
 
         }
 
